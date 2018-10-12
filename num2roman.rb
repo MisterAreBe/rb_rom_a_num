822a44fc8049cf0a41d05a222cf3b032cfba30d3
Ricky Collins <richardcollins52317@gmail.com>
2018-10-12 14:53:49 -0400

    test passed

--- num2roman.rb
+++ num2roman.rb
@@ -2,12 +2,14 @@ class Integer
 
 	def romanize()
 		roman_numerals = {
+			'IV' => 4,
 			'I' => 1
 		}
 		x = self
 		roman = ''
 		roman_numerals.each do |key, value|
-			roman << key*(x/value)
+			roman << key * (x / value)
+			x = x % value
 		end
 		return roman
 	end
