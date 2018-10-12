854aa03c3cb19907c823a2395e7300e346f073aa
Ricky Collins <richardcollins52317@gmail.com>
2018-10-12 15:32:21 -0400

    test failed

--- test_roman2num.rb
+++ test_roman2num.rb
@@ -6,5 +6,9 @@ class TestRoman2Num < Minitest::Test
     def test_assert_that_1_equals_1
         assert_equal(1, 1)
     end
-    
+
+    def test_return_is_integer
+        assert_equal(Integer, ''.arabic_num().class)
+    end
+
 end
