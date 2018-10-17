7037e149ffbea902b9d83fe0627906c2d2a95890
Ricky Collins <richardcollins52317@gmail.com>
2018-10-17 11:35:06 -0400

    got part of front end start, able to pass numbers and receive roman numerals

--- app.rb
+++ app.rb
@@ -4,5 +4,12 @@ require_relative 'require_me.rb'
 enable :sessions
 
 get '/' do
-  erb :index, :layout => :layout 
+  roman_numeral = session[:roman_numeral] || ''
+  erb :index, :layout => :layout, locals: {roman_numeral: roman_numeral}
+end
+
+post '/romanize' do
+  temp = params[:to_roman].to_i
+  session[:roman_numeral] = temp.romanize()
+  redirect '/'
 end
